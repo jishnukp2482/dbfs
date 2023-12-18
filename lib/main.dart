96f087c13f0b9731505cf15ee99d6553301f8268
jishnukp2482 <jishnukpvidyadhran123@gmail.com>
2023-12-18 11:43:07 +0530

    dbfs

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid) {}
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'dbfs ',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final WebViewController webViewController;
+  @override
+  void initState() {
+    webViewController = WebViewController()
+      ..setBackgroundColor(Colors.white)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://nbfc.dbfsindia.com/ogl/login'))
+      ..setNavigationDelegate(NavigationDelegate(
+        onProgress: (progress) {
+          const Center(child: CircularProgressIndicator());
+        },
+      ));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Card(child: WebViewWidget(controller: webViewController))),
+    );
+  }
+}
