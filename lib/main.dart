a10e5dace9f8c509898011aeb50d15592d5a3c87
jishnukp2482 <jishnukpvidyadhran123@gmail.com>
2023-12-18 11:59:05 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isAndroid) {}
   runApp(const MyApp());
 }
 
